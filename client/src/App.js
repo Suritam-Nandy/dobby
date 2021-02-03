14bcfe7d301de81108f62e864748c54e6385048c
Suritam-Nandy <13suritsuritam@gmail.com>
2021-02-03 18:53:26 +0530

    🎉

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bootstrapped using <code>Springboard</code>
+          ok
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/srm-kzilla/springboard"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Star the repo. Show the love.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
